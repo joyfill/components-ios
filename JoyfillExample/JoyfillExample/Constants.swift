e677753bfc49b7e43a186b82ec1a43865868281b
Jeremy Pagley <jeremyjkp@gmail.com>
2023-11-15 12:57:52 -0500

    0.2.0 version Bump

--- JoyfillExample/JoyfillExample/Constants.swift
+++ JoyfillExample/JoyfillExample/Constants.swift
@@ -3,8 +3,10 @@ import Foundation
 
 struct Constants {
     // MARK: - API
+    
+    // Documents endpoint https://docs.joyfill.io/reference/overview-documents
     static let baseURL = "https://api-joy.joyfill.io/v1/documents"
     
-    // https://docs.joyfill.io/docs/authentication#user-access-tokens
+    // See https://docs.joyfill.io/docs/authentication#user-access-tokens
     static let userAccessToken = "replace_me"
 }
