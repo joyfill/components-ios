7da4bf0b4553376008aa3e173a9b1a10cae70098
Jeremy Pagley <jeremyjkp@gmail.com>
2023-12-19 22:20:16 -0500

    Work for Example

---
+++ examples/JoyfillExampleUIKit/JoyfillExampleUIKit/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  JoyfillExampleUIKit
+//
+//  Created by Jeremy Pagley on 12/19/23.
+//
+
+import Foundation
---
+++ examples/JoyfillExampleUIKit/JoyfillExampleUIKit/Models/JoyDocViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  JoyDocViewModel.swift
+//  JoyfillExampleUIKit
+//
+//  Created by Jeremy Pagley on 12/19/23.
+//
+
+import Foundation
