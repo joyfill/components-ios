1e23d9148fd7add20a13b6129dc7975717b2d551
Jeremy Pagley <jeremyjkp@gmail.com>
2024-01-11 11:12:29 -0500

    FInal prep

--- examples/JoyfillExampleUIKit/JoyfillExampleUIKit/Constants.swift
+++ examples/JoyfillExampleUIKit/JoyfillExampleUIKit/Constants.swift
@@ -7,6 +7,6 @@ struct Constants {
     static let baseURL = "https://api-joy.joyfill.io/v1/documents"
     
     // See https://docs.joyfill.io/docs/authentication#user-access-tokens
-    static let userAccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6IjY0MjIyMjU0NjE2NzcyZmFmYzdhNzc2MiJ9.vjw0KYOQ6hlo98ar7BDz-6ADYZndV4hwFRF4XAQtz58"
+    static let userAccessToken = "<replace_me"
     
 }
